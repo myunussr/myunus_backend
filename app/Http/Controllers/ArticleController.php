429f18d68c0763e23e9365600c015f9debd735e3
myunussr <myunusrukmana@email.unikom.ac.id>
2022-01-07 12:41:00 +0700

    Create Article and Auth Controller

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Article;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use App\Http\Requests\ArticleRequest;
+
+class ArticleController extends Controller
+{
+
+    public function index()
+    {
+        $articles = Article::all();
+
+        return response()->json([
+            "success" => true,
+            "message" => "List Articles",
+            "data" => $articles
+        ]);
+    }
+
+
+    public function store(ArticleRequest  $request)
+    {
+        $data = $request->all();
+
+        $data['slug'] = Str::slug($request->title);
+        $article = Article::create($data);
+
+        return response()->json([
+            "success" => true,
+            "message" => "Article created successfully.",
+            "data" => $article
+        ]);
+    }
+
+
+    public function show($id)
+    {
+        return response()->json([
+            "success" => true,
+            "message" => "Article retrieved successfully.",
+            "data" => Article::find($id)
+        ]);
+    }
+
+    public function update(ArticleRequest $request, Article $article)
+    {
+        $data = $request->all();
+
+        $article = Article::findOrFail($article->id);
+        $data['slug'] = Str::slug($request->title);
+        $article->update($data);
+
+        return response()->json([
+            "success" => true,
+            "message" => "Article updated successfully.",
+            "data" => $article
+        ]);
+    }
+
+
+    public function destroy(Article $article)
+    {
+        $article = Article::findOrFail($article->id);
+        $article->delete();
+
+        return response()->json([
+            "success" => true,
+            "message" => "Article deleted successfully.",
+            "data" => $article
+        ]);
+    }
+}
---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Requests\UserRequest;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function register(UserRequest $request)
+    {
+        $data = $request->all();
+
+        $data['password'] = bcrypt($request->password);
+        $user =  User::create($data);
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'data' => $user,
+            'access_token' => $token,
+            'token_type' => 'Bearer'
+        ]);
+    }
+
+    public function login(Request $request)
+    {
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
+
+        $user = User::where('email', $request['email'])->firstOrFail();
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'Hi ' . $user->name . ', welcome to home',
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ]);
+    }
+
+    public function logout()
+    {
+        Auth::user()->tokens()->delete();
+        return ['message' => 'You have successfully logged out'];
+    }
+}
