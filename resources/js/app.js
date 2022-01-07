fc7412cbe44bfd87f62ceb69abb9a26e332c127a
myunussr <myunusrukmana@email.unikom.ac.id>
2022-01-07 14:11:44 +0700

    View List Articles

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,43 @@
 require('./bootstrap');
+
+import Vue from 'vue/dist/vue';
+
+window.Vue = Vue;
+
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
+import VueAxios from 'vue-axios';
+import axios from 'axios';
+
+import App from './App.vue';
+Vue.use(VueAxios, axios);
+
+import IndexComponent from './components/articles/Index.vue';
+import CreateComponent from './components/articles/Create.vue';
+// import EditComponent from './components/articles/Edit.vue';
+
+const routes = [
+    {
+        name: 'articles',
+        path: '/',
+        component: IndexComponent
+    },
+    {
+        name: 'create',
+        path: '/create',
+        component: CreateComponent
+    },
+    // {
+    //     name: 'edit',
+    //     path: '/edit/:id',
+    //     component: EditComponent
+    // }
+];
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: routes
+});
+
+const app = new Vue(Vue.util.extend({ router }, App)).$mount('#app');
---
+++ resources/js/components/articles/Index.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="container mt-3">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">Articles</div>
+                    <div class="card-body">
+                        <router-link :to="{ name: 'create' }" class="btn btn-md btn-success">Add Article</router-link>
+                        <div class="row">
+                            <div class="col-auto d-flex flex-wrap align-items-center">
+                                <div class="card m-3" style="width: 18rem;" v-for="(article, index) in articles" :key="article.id">
+                                    <div class="card-body" >
+                                        <h5 class="card-title">{{ article.title }}</h5>
+                                        <h6 class="card-subtitle mb-2 text-muted">{{ article.title }}</h6>
+                                        <p class="card-text">{{ article.excerpt }}</p>
+                                        <a href="#" class="card-link">Detail</a>
+                                        <a href="#" class="card-link">Remove</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                articles: []
+            }
+        },
+        created() {
+            let uri = `http://localhost:8000/api/articles`;
+            this.axios.get(uri).then(response => {
+                this.articles = response.data.data;
+            });
+        }
+    }
+</script>
